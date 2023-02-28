3b628b524951ce807a3f38c647e8cf2f96185fb8
Santiago Montero <santiagojmontero@outlook.com>
2023-02-27 21:58:11 -0300

    starter css code cleanup and using of a proper logo for the app

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # React-Pokedex
 
 ... WORK IN PROGRESS
+
+
+### DISCLAIMER
+This is a experimental application doing on my own for study purposes. Trademarks and copyrights for third-party the Pokemon games and characters are owned by NINTENDO.
--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="app">
+      <header className="app-header">
+        <img src={logo} className="app-logo" alt="logo" />
+        <p style={{"fontFamily": "roboto"}}>
+          This is our app with the logo and UI library ready
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
